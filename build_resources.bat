2b67143ba2eb28c4d25bd6ccbe2e5b54ae6a6696
henrypp <support@henrypp.org>
2017-12-29 20:22:13 +0600

    fixes

--- build_resources.bat
+++ build_resources.bat
@@ -2,5 +2,7 @@
 
 php "..\builder\make_resource.php" ".\src\resource.hpp"
 php "..\builder\make_locale.php" "Error Lookup" "errorlookup" ".\bin\i18n" ".\src\resource.hpp" ".\bin\errorlookup.lng"
+copy /y ".\bin\errorlookup.lng" ".\bin\32\errorlookup.lng"
+copy /y ".\bin\errorlookup.lng" ".\bin\64\errorlookup.lng"
 
 pause
