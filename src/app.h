acbb533d7c19102f317e75cf4b87d7e331b12a8a
henrypp <support@henrypp.org>
2020-12-06 19:54:01 +0600

    ++version

---
+++ src/app.h
@@ -0,0 +1,10 @@
+// // Error Lookup
+// Copyright (c) 2011-2021 Henry++
+
+#pragma once
+
+#define APP_NAME L"Error Lookup"
+#define APP_NAME_SHORT L"errorlookup"
+#define APP_VERSION L"2.4"
+#define APP_VERSION_RES 2,4,0,0
+#define APP_COPYRIGHT L"(c) 2011-2021 " APP_AUTHOR L". All Rights Reversed."
