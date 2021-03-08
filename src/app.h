0e59c3f4fdc4cc93249c65bb9333e642c810f3cc
henrypp <support@henrypp.org>
2021-03-08 20:17:47 +0600

    ++version

--- src/app.h
+++ src/app.h
@@ -5,7 +5,7 @@
 
 #define APP_NAME L"Error Lookup"
 #define APP_NAME_SHORT L"errorlookup"
-#define APP_VERSION L"2.4.1"
-#define APP_VERSION_RES 2,4,1,0
+#define APP_VERSION L"2.4.2"
+#define APP_VERSION_RES 2,4,2,0
 #define APP_AUTHOR L"Henry++"
 #define APP_COPYRIGHT L"(c) 2011-2021 " APP_AUTHOR L". All Rights Reversed."
