9ba3d3fc3d558f8d3a171aea63a97dd1de0d86f8
henrypp <support@henrypp.org>
2022-01-23 03:52:35 +0600

    ++version

--- src/app.h
+++ src/app.h
@@ -1,11 +1,11 @@
 // Error Lookup
-// Copyright (c) 2011-2021 Henry++
+// Copyright (c) 2011-2022 Henry++
 
 #pragma once
 
 #define APP_NAME L"Error Lookup"
 #define APP_NAME_SHORT L"errorlookup"
-#define APP_VERSION L"2.4.2"
-#define APP_VERSION_RES 2,4,2,0
+#define APP_VERSION L"2.4.3"
+#define APP_VERSION_RES 2,4,3,0
 #define APP_AUTHOR L"Henry++"
-#define APP_COPYRIGHT L"(c) 2011-2021 " APP_AUTHOR L". All Rights Reversed."
+#define APP_COPYRIGHT L"(c) 2011-2022 " APP_AUTHOR L". All Rights Reversed."
