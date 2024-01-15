921de31b100883c155d288ac696621b7ef02792f
henrypp <sforce5@mail.ru>
2024-01-16 05:31:56 +0600

    upd app.h

--- src/app.h
+++ src/app.h
@@ -1,11 +1,11 @@
 // Error Lookup
-// Copyright (c) 2011-2023 Henry++
+// Copyright (c) 2011-2024 Henry++
 
 #pragma once
 
 #define APP_NAME L"Error Lookup"
 #define APP_NAME_SHORT L"errorlookup"
-#define APP_VERSION L"2.4.3"
-#define APP_VERSION_RES 2,4,3,0
+#define APP_VERSION L"2.5"
+#define APP_VERSION_RES 2,5,0,0
 #define APP_AUTHOR L"Henry++"
-#define APP_COPYRIGHT L"(c) 2011-2023 " APP_AUTHOR L". All Rights Reversed."
+#define APP_COPYRIGHT L"(c) 2011-2024 " APP_AUTHOR L". All Rights Reversed."
