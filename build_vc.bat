d265967ab45f6ca5f32a56bf1dac7b6091b09303
henrypp <sforce5@mail.ru>
2026-02-16 23:12:09 +0500

    fuck off 32-bit

--- build_vc.bat
+++ build_vc.bat
@@ -20,9 +20,6 @@ goto end
 
 :start
 
-msbuild errorlookup.sln -property:Configuration=Release -property:Platform=x86 -verbosity:normal
-if %ERRORLEVEL% neq 0 goto end
-
 msbuild errorlookup.sln -property:Configuration=Release -property:Platform=x64 -verbosity:normal
 if %ERRORLEVEL% neq 0 goto end
 
